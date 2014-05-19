22b947f3a79678487f0a76e5e8822f134ca75f61
Henrique Mello Cholodovskis Filho <henriquecholo@gmail.com>
2014-05-18 23:35:12 -0300

    Following tutorial to make the three way data-binding

---
+++ app/scripts/app.js
@@ -0,0 +1,24 @@
+'use strict';
+var app = angular
+  .module('angularApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    'firebase'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .when('/posts/:postId', {
+        templateUrl: 'views/showpost.html',
+        controller: 'PostViewCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+  .constant('FIREBASE_URL', 'https://flickering-fire-1311.firebaseio.com');
